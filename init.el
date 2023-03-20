ae07bf73a5b26a4157a9119f4743ddf5d2347d1e
kix6 <kix6@outlook.com>
2023-03-20 12:53:12 +0800

    2023/3/20 update

--- init.el
+++ init.el
@@ -5,18 +5,20 @@
 (tooltip-mode -1)
 (set-fringe-mode 10)
 
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "#CACFD2") 
+(set-face-foreground 'font-lock-comment-face "#CD6155")
+(set-face-background 'font-lock-comment-face "white")
 (menu-bar-mode -1)
 
 (setq visible-bell t)
-
-(require 'package)
 
 ;;; initialize package sources
 (require 'package)
 
 (setq package-archives '(("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu//")
-			 ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-			 ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org//")))
+			 ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+
 
 (package-initialize)
 (unless package-archive-contents
@@ -39,6 +41,9 @@
 		eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
+(use-package treemacs
+  :bind ("M-0" . treemacs-select-window))
+
 ;;; key-bindings
 (global-set-key (kbd "C-M-m") 'set-mark-command)
 
@@ -49,68 +54,68 @@
 (use-package swiper)
 
 (use-package ivy
-	     :diminish
-	     :bind (("C-s" . swiper)
-		    :map ivy-minibuffer-map
-		    ("TAB" . ivy-alt-done)
-		    ("C-l" . ivy-alt-done)
-		    ("C-j" . ivy-next-line)
-		    ("C-k" . ivy-previous-line)
-		    :map ivy-switch-buffer-map
-		    ("C-k" . ivy-previous-line)
-		    ("C-l" . ivy-done)
-		    ("C-d" . ivy-switch-buffer-kill)
-		    :map ivy-reverse-i-search-map
-		    ("C-k" . ivy-previous-line)
-		    ("C-d" . ivy-reverse-i-search-kill))
-	     :config
-	     (ivy-mode 1))
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
 
 (use-package doom-modeline
-	     :init (doom-modeline-mode 1)
-	     :custom ((doom-modeline-height 15)))
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
 
 (use-package doom-themes
-	     :init (load-theme 'doom-acario-dark t))
+  :init (load-theme 'doom-acario-light t))
 
 (use-package rainbow-delimiters
-	     :hook (prog-mode . rainbow-delimiters-mode))
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package which-key
-	     :init (which-key-mode)
-	     :diminish which-key-mode
-	     :config
-	     (setq which-key-idle-delay 1))
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
 
 (use-package counsel
-	     :bind (("M-x" . counsel-M-x)
-		    ("C-x b" . counsel-ibuffer)
-		    ("C-x C-f" . counsel-find-file)
-		    :map minibuffer-local-map
-		    ("C-r" . 'counsel-minibuffer-history)))
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
 
 (use-package ivy-rich
-	     :init
-	     (ivy-rich-mode 1))
+  :init
+  (ivy-rich-mode 1))
 
 (use-package ivy-prescient
-	     :after counsel
-	     :custom
-	     (ivy-prescient-enable-filtering nil)
-	     :config
-	     ;; Uncomment the following line to have sorting remembered across sessions!
+  :after counsel
+  :custom
+  (ivy-prescient-enable-filtering nil)
+  :config
+  ;; Uncomment the following line to have sorting remembered across sessions!
 					;(prescient-persist-mode 1)
-	     (ivy-prescient-mode 1))
+  (ivy-prescient-mode 1))
 
 (use-package helpful
-	     :custom
-	     (counsel-describe-function-function #'helpful-callable)
-	     (counsel-describe-variable-function #'helpful-variable)
-	     :bind
-	     ([remap describe-function] . counsel-describe-function)
-	     ([remap describe-command] . helpful-command)
-	     ([remap describe-variable] . counsel-describe-variable)
-	     ([remap describe-key] . helpful-key))
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
 
 (defun org-mode-setup ()
   (org-indent-mode)
@@ -145,118 +150,173 @@
   (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
 
 (use-package org
-	     :hook (org-mode . org-mode-setup)
-	     :config
-	     (setq org-ellipsis " ▾"
-		   org-hide-emphasis-markers t)
+  :hook (org-mode . org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"
+	org-hide-emphasis-markers t)
 
-	     (setq org-agenda-start-with-log-mode t)
-	     (setq org-log-done 'time)
-	     (setq org-log-into-drawer t)
+  (setq org-agenda-start-with-log-mode t)
+  (setq org-log-done 'time)
+  (setq org-log-into-drawer t)
 
-	     (require 'org-habit)
-	     
-	     (setq org-agenda-files
-		   '("~/GTD/inbox.org"
-		     "~/GTD/gtd.org"
-		     "~/GTD/tickler.org"))
-	     
+  (require 'org-habit)
+  
+  (setq org-agenda-files
+	'("~/GTD/inbox.org"
+	  "~/GTD/gtd.org"
+	  "~/GTD/tickler.org"))
+  
 
-	     (setq org-todo-keywords
-		   '((sequence "TODO(t)" "NEXT(n)" "WAITING(w)" "|" "DONE(d!) CANCLED(c)")))
-	     
-	     (setq org-refile-targets
-		   '(("~/GTD/gtd.org" :maxlevel . 3)
-		     ("~/GTD/someday.org" :level . 1)
-		     ("~/GTD/tickler.org" :maxlevel . 2)))
-	     
-	     (advice-add 'org-refile :after 'org-save-all-org-buffers)
+  (setq org-todo-keywords
+	'((sequence "TODO(t)" "NEXT(n)" "WAITING(w)" "|" "DONE(d!) CANCLED(c)")))
+  
+  (setq org-refile-targets
+	'(("~/GTD/gtd.org" :maxlevel . 3)
+	  ("~/GTD/someday.org" :level . 1)
+	  ("~/GTD/tickler.org" :maxlevel . 2)))
+  
+  (advice-add 'org-refile :after 'org-save-all-org-buffers)
 
-	     (setq org-capture-templates
-		   `(("t" "Todo [inbox]" entry
-		      (file+headline "~/GTD/inbox.org" "Tasks")
-		      "* TODO %i%?")
-		     ("T" "Tickler" entry
-		      (file+headline "~/GTD/tickler.org" "Tickler")
-		      "* %i%? \n %U")))
-	     
-	     (define-key global-map (kbd "C-c c")
-	       (lambda () (interactive) (org-capture)))
-	       
-	     (org-font-setup))
+  (setq org-capture-templates
+	`(("t" "Todo [inbox]" entry
+	   (file+headline "~/GTD/inbox.org" "Tasks")
+	   "* TODO %i%?")
+	  ("T" "Tickler" entry
+	   (file+headline "~/GTD/tickler.org" "Tickler")
+	   "* %i%? \n %U")))
+  
+  (define-key global-map (kbd "C-c c")
+	      (lambda () (interactive) (org-capture)))
+  
+  (org-font-setup))
 
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  (setq org-roam-dailies-directory "journal/")
+  (setq org-roam-dailies-capture-templates
+	'(("d" "default" entry "* %<%I:%M %p>: %?"
+	   :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
+  
+  :custom
+  (org-roam-directory "~/RoamNotes")
+  (org-roam-completion-everywhere t)
+  (org-roam-capture-templates
+   '(("d" "default" plain
+      "%?"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+      :unnarrowed t)
+     ("p" "paper notes" plain
+      "\n* Source\n\nAuthor: %^{Author}\nTitle: ${title}\nYear: %^{Year}\n\n* Summary\n\n%?"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+      :unnarrowed t)))
+  
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n i" . org-roam-node-insert)
+	 :map org-mode-map
+	 ("C-M-i" . completion-at-point)
+	 :map org-roam-dailies-map
+	 ("Y" . org-roam-dailies-capture-yesterday)
+	 ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+  :config
+  (require 'org-roam-dailies)
+  (org-roam-db-autosync-mode)
+  (org-roam-setup))
 
 (use-package org-bullets
-	     :after org
-	     :hook (org-mode . org-bullets-mode)
-	     :custom
-	     (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
 (use-package projectile
-	     :diminish projectile-mode
-	     :config (projectile-mode)
-	     :custom (projectile-completion-system 'ivy)
-	     :bind-keymap
-	     ("C-c p" . projectile-command-map)
-	     :init
-	     (setq projectile-switch-project-action #'projectile-dired))
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom (projectile-completion-system 'ivy)
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (setq projectile-switch-project-action #'projectile-dired))
 
 (use-package counsel-projectile
-	     :config (counsel-projectile-mode))
+  :config (counsel-projectile-mode))
 
 (use-package magit
-	     :commands magit-status
-	     :custom
-	     (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+  :commands magit-status
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
 (defun kix6/lsp-mode-setup ()
   (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
   (lsp-headerline-breadcrumb-mode))
 
 (use-package lsp-mode
-	     :commands (lsp lsp-deferred)
-	     :hook
-	     (lsp-mode . kix6/lsp-mode-setup)
-	     (c-mode . lsp)
-	     (c++-mode . lsp)
-	     :init
-	     (setq lsp-keymap-prefix "C-c l")
-	     :config
-	     (lsp-enable-which-key-integration t))
+  :commands (lsp lsp-deferred)
+  :hook
+  (lsp-mode . kix6/lsp-mode-setup)
+  (c-mode . lsp)
+  (c++-mode . lsp)
+  :custom
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  ;; enable / disable the hints as you prefer:
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  (lsp-enable-which-key-integration t))
 
 (use-package lsp-ui
-	     :hook (lsp-mode . lsp-ui-mode)
-	     :custom
-	     (lsp-ui-doc-position 'bottom))
+  :ensure
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-position 'bottom)
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover nil)
+  (lsp-ui-doc-enable nil)
+  (lsp-ui-imenu-enable t)
+  (lsp-ui-sideline-enable nil))
+
 
 (use-package lsp-treemacs
-	     :after lsp
-	     :config
-	     (lsp-treemacs-sync-mode 1))
+  :after lsp
+  :config
+  (lsp-treemacs-sync-mode 1))
 
 (use-package lsp-ivy
-	     :after lsp)
+  :after lsp)
 
 (use-package company
-	     :after lsp-mode
-	     :hook (lsp-mode . company-mode)
-	     :bind
-	     (:map company-active-map
-		   ("<tab>" . company-complete-selection))
-	     (:map lsp-mode-map
-		   ("<tab>" . company-indent-or-complete-common))
-	     :custom
-	     (company-minimum-prefix-length 1)
-	     (company-idle-delay 0.0))
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind
+  (:map company-active-map
+	("<tab>" . company-complete-selection))
+  (:map lsp-mode-map
+	("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
 
 (use-package company-box
-	     :hook (company-mode . company-box-mode))
+  :hook (company-mode . company-box-mode))
 
 ;;; configuration for scala
 
 (use-package scala-mode
-	     :interpreter
-	     ("scala" . scala-mode))
+  :interpreter
+  ("scala" . scala-mode))
 
 (use-package lsp-metals)
 
@@ -264,12 +324,14 @@
 ;;; configuration for terminals
 
 (use-package term
-	     :config
-	     (setq explicit-shell-file-name "bash")
-	     (setq term-prompt-regexp "^[^#?%>\n]*[#$%] *"))
+  :config
+  (setq explicit-shell-file-name "bash")
+  (setq term-prompt-regexp "^[^#?%>\n]*[#$%] *"))
 
 (use-package eterm-256color
-	     :hook (term-mode . eterm-256color-mode))
+  :hook (term-mode . eterm-256color-mode))
+
+(use-package clang-format)
 
 (defun kix6/configure-eshell ()
   (add-hook 'eshell-pre-command-hook 'eshell-save-some-history)
@@ -283,12 +345,55 @@
 (use-package eshell-git-prompt)
 
 (use-package eshell
-	     :hook (eshell-first-time-mode . kix6/configure-eshell)
-	     :config
-	     (eshell-git-prompt-use-theme 'robbyrussell)
-	     (with-eval-after-load 'esh-opt
-	       (setq eshell-destroy-buffer-when-process-dies t)
-	       (setq eshell-visual-commands '("top" "zsh" "vim" "htop"))))
+  :hook (eshell-first-time-mode . kix6/configure-eshell)
+  :config
+  (eshell-git-prompt-use-theme 'robbyrussell)
+  (with-eval-after-load 'esh-opt
+    (setq eshell-destroy-buffer-when-process-dies t)
+    (setq eshell-visual-commands '("top" "zsh" "vim" "htop"))))
+
+;;; for OCaml
+(use-package tuareg
+  :hook merlin-mode)
+
+(use-package merlin)
+(use-package merlin-company)
+
+;;; ace-window
+(use-package ace-window
+  :ensure
+  :bind
+  ("M-o" . ace-window)
+  :delight
+  :config
+  (ace-window-display-mode 1))
+
+(use-package git-timemachine)
+
+(use-package undo-tree)
+
+;;; config for rust
+(use-package rustic
+  :ensure
+  :bind (:map rustic-mode-map
+	      ("M-j" . lsp-ui-imenu)
+	      ("M-?" . lsp-find-references)
+	      ("C-c C-c l" . flycheck-list-errors)
+	      ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status))
+  :hook
+  (rustic-mode-hook . rk/rustic-mode-hook)
+  :custom
+  (rustic-format-on-save t))
+
+(defun rk-rustic-mode-hook ()
+  (when buffer-file-name
+    (setq-local buffer-save-without-query t))
+  (add-hook 'before-save-hook 'lsp-format-buffer nil t))
+
+(use-package tramp)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -297,10 +402,13 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (eshell-git-prompt ansi-term-mode ansi-term eterm-256color which-key use-package rainbow-delimiters org-bullets magit lsp-ui lsp-metals lsp-ivy ivy-rich ivy-prescient helpful doom-themes doom-modeline counsel-projectile company-box))))
+    (rustic flycheck undo-tree git-timemachine merlin-company merlin tuareg clang-format eshell-git-prompt ansi-term-mode ansi-term eterm-256color which-key use-package rainbow-delimiters magit lsp-ui lsp-metals lsp-ivy ivy-rich ivy-prescient helpful doom-themes doom-modeline counsel-projectile company-box))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
