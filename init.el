9a585a552dca7627becbdb84d1b31e2a16c513b1
kix6 <kix6@outlook.com>
2024-04-10 18:26:51 +0800

    better remote editing

--- init.el
+++ init.el
@@ -52,7 +52,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(eyebrowse neotree treesit-auto sort-tab ledger-mode ob-ledger utop flycheck-ocaml merlin-eldoc merlin-mode ocp-indent json-mode flycheck undo-tree git-timemachine merlin-company merlin tuareg clang-format eshell-git-prompt ansi-term-mode ansi-term eterm-256color which-key use-package rainbow-delimiters magit lsp-ui lsp-metals lsp-ivy ivy-rich ivy-prescient helpful doom-themes doom-modeline counsel-projectile company-box)))
+   '(eyebrowse neotree treesit-auto sort-tab ledger-mode ob-ledger utop flycheck-ocaml merlin-eldoc merlin-mode ocp-indent json-mode flycheck undo-tree git-timemachine merlin-company merlin tuareg clang-format eshell-git-prompt ansi-term-mode ansi-term eterm-256color which-key use-package rainbow-delimiters magit lsp-ui lsp-metals lsp-ivy ivy-rich ivy-prescient helpful doom-themes doom-modeline counsel-projectile company-box))
+ '(safe-local-variable-values
+   '((eval add-to-list 'eglot-server-programs
+	   '(python-ts-mode "/home/kix6/.miniconda3/envs/SMAC/bin/pyright-langserver" "--stdio"))
+     (eval
+      (add-to-list 'eglot-server-programs
+		   '(python-ts-mode "/home/kix6/.miniconda3/envs/SMAC/bin/pyright-langserver" "--stdio")))
+     (demo . "this is a demo"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
