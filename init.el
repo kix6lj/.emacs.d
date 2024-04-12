79790542ce181fc1b30f3c9953523405d56e62e4
kix6 <kix6@outlook.com>
2024-04-12 14:39:17 +0800

    configure for corfu

--- init.el
+++ init.el
@@ -31,7 +31,6 @@
   (package-install 'use-package))
 
 (require 'use-package)
-(setq use-package-always-ensure t)
 
 (column-number-mode)
 (global-display-line-numbers-mode t)
@@ -53,8 +52,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-show-quick-access t nil nil "Customized with use-package company")
  '(package-selected-packages
-   '(marginalia corfu consult orderless vertico eyebrowse neotree treesit-auto sort-tab ledger-mode ob-ledger utop flycheck-ocaml merlin-eldoc merlin-mode ocp-indent json-mode flycheck undo-tree git-timemachine merlin-company merlin tuareg clang-format eshell-git-prompt ansi-term-mode ansi-term eterm-256color which-key use-package rainbow-delimiters magit lsp-ui lsp-metals lsp-ivy ivy-rich ivy-prescient helpful doom-themes doom-modeline counsel-projectile company-box))
+   '(corfu-popupinfo corfu-info vertico-directory marginalia corfu consult orderless vertico eyebrowse neotree treesit-auto sort-tab ledger-mode ob-ledger utop flycheck-ocaml merlin-eldoc merlin-mode ocp-indent json-mode flycheck undo-tree git-timemachine merlin-company merlin tuareg clang-format eshell-git-prompt ansi-term-mode ansi-term eterm-256color which-key use-package rainbow-delimiters magit lsp-ui lsp-metals lsp-ivy ivy-rich ivy-prescient helpful doom-themes doom-modeline counsel-projectile company-box))
  '(safe-local-variable-values
    '((eval add-to-list 'eglot-server-programs
 	   '(python-ts-mode "/home/kix6/.miniconda3/envs/SMAC/bin/pyright-langserver" "--stdio"))
