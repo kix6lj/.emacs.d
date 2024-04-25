0bd737bf7eef6c9ebd63aad0a6aa06a1a242e019
kix6 <kix6@outlook.com>
2024-04-25 10:24:29 +0800

    add pdf-tools

--- init.el
+++ init.el
@@ -40,7 +40,8 @@
 		term-mode-hook
 		shell-mode-hook
 		treemacs-mode-hook
-		eshell-mode-hook))
+		eshell-mode-hook
+		pdf-view-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;;; set emacs backup file directory
@@ -54,7 +55,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(embark-consult embark dashboard emacs-dashboard corfu-popupinfo corfu-info vertico-directory marginalia corfu consult orderless vertico eyebrowse neotree treesit-auto sort-tab ledger-mode ob-ledger utop flycheck-ocaml merlin-eldoc merlin-mode ocp-indent json-mode flycheck undo-tree git-timemachine merlin tuareg clang-format eshell-git-prompt ansi-term-mode ansi-term eterm-256color which-key use-package rainbow-delimiters magit lsp-ui lsp-metals helpful doom-themes doom-modeline))
+   '(pdf-tools embark-consult embark dashboard emacs-dashboard corfu-popupinfo corfu-info vertico-directory marginalia corfu consult orderless vertico eyebrowse neotree treesit-auto sort-tab ledger-mode ob-ledger utop flycheck-ocaml merlin-eldoc merlin-mode ocp-indent json-mode flycheck undo-tree git-timemachine merlin tuareg clang-format eshell-git-prompt ansi-term-mode ansi-term eterm-256color which-key use-package rainbow-delimiters magit lsp-ui lsp-metals helpful doom-themes doom-modeline))
  '(safe-local-variable-values
    '((eval add-to-list 'eglot-server-programs
 	   '(python-ts-mode "/home/kix6/.miniconda3/envs/SMAC/bin/pyright-langserver" "--stdio"))
